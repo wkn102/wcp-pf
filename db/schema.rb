f701979218322ce2aa26686f318a73d8986d4ca4
wkn102 <lino.libra1002@gmail.com>
2021-10-26 12:57:21 +0000

    [Fix] migration

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,6 @@ ActiveRecord::Schema.define(version: 2021_10_25_114526) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
