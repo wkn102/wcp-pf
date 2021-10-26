7f11262f64484aea6c62053eefae399d3a8ebf9a
wkn102 <lino.libra1002@gmail.com>
2021-10-26 13:00:19 +0000

    [Fix] migration

--- db/migrate/20211014081254_remove_location_id_from_posts.rb
+++ db/migrate/20211014081254_remove_location_id_from_posts.rb
@@ -1,5 +1,5 @@
 class RemoveLocationIdFromPosts < ActiveRecord::Migration[5.2]
   def change
-    remove_column :posts, :Location_id, :integer
+    # remove_column :posts, :Location_id, :integer
   end
 end
